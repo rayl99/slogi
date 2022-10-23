660b7dfa5c7b17aa00e4ff0cd97e6b109ea2fbb5
Võ Ngọc Huy <53592062+rayl99@users.noreply.github.com>
2022-10-23 13:32:42 +0700

    Delete queue_collection directory

--- queue_collection/__init__.py
+++
@@ -1,6 +0,0 @@
-from queue_collection.circular_queue import CircularQueue
-from queue_collection.deque import Deque
-from queue_collection.priority_queue import PriorityQueue
-from queue_collection.queue import Queue
-
-__all__ = ['CircularQueue', 'Deque', 'PriorityQueue', 'Queue']
